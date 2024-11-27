d5c3b39a392c2b454033b16a18bbb740e8672bfe
yongin <rladls6412@naver.com>
2024-11-27 13:59:08 +0900

    design #4 : 회원가입 페이지 디자인 완료

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,49 @@
+import Link from "next/link";
 import style from "./SignupPage.module.scss";
 
 function SignupPage() {
-  return <div>회원가입 페이지</div>;
+  return (
+    <div className={style.signup_container}>
+      <div className={style.form_container}>
+        <h3>회원가입</h3>
+        <form className={style.signup_form_box}>
+          <div className={style.signup_input_box}>
+            <label>이름</label>
+            <div>
+              <input placeholder="이름을 입력해 주세요" />
+            </div>
+          </div>
+          <div className={style.signup_input_box}>
+            <label>이메일</label>
+            <div>
+              <input placeholder="이메일을 입력해 주세요" />
+            </div>
+          </div>
+          <div className={style.signup_input_box}>
+            <label>비밀번호</label>
+            <div>
+              <input placeholder="비밀번호를 입력해 주세요" />
+            </div>
+          </div>
+          <div className={style.signup_input_box}>
+            <label>비밀번호 확인</label>
+            <div>
+              <input placeholder="비밀번호를 입력해 주세요" />
+            </div>
+          </div>
+          <div>
+            <button className={style.btn}>가입하기</button>
+          </div>
+        </form>
+      </div>
+      <p>
+        이미 회원이신가요?
+        <span>
+          <Link href={"/login"}>로그인하기</Link>
+        </span>
+      </p>
+    </div>
+  );
 }
 
 export default SignupPage;
